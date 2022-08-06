5d84a038f5ae6fc309a235337b66e24b5e917b73
xihuo <799616380@qq.com>
2022-08-06 16:45:36 +0800

    更改表单

--- main.py
+++ main.py
@@ -45,18 +45,21 @@ def sign(school_id, password, auto_position, vaccine):
             url = 'http://yqdj.zucc.edu.cn/feiyan_api/examen/examenSchemeController/findExamenSchemeById.do'
             r = requests.post(url, cookies=cookies, data={'esId': 2}, timeout=30)
             questions = json.loads(r.json()['data']['examen']['scheme'])['questions']
-
+            # print(questions)
             # 填写表单并提交
-            with open("./form.json", "r", encoding='utf-8') as f:
+            with open("./form_2.json", "r", encoding='utf-8') as f:
                 form = json.load(f)
                 if form['questions'] != questions:
+                    # print(form['questions'])
+                    # print(questions)
                     return "打卡表单已更新，当前版本不可用"
 
                 answer = form['answer']
                 answer["填报日期(Date)"] = str(
                     datetime.datetime.now(tz=datetime.timezone(datetime.timedelta(hours=8))).date())
                 answer["自动定位(Automatic location)"] = auto_position
-                answer["疫苗接种情况?(Vaccination status?)"] = vaccine
+                answer["当前疫苗接种情况? (Vaccination status?)"] = vaccine
+                print(answer)
                 data = json.dumps({"examenSchemeId": 2, "examenTitle": "师生报平安", "answer": answer})
                 headers = {'Content-Type': 'application/json'}
                 url = "http://yqdj.zucc.edu.cn/feiyan_api/examen/examenAnswerController/commitAnswer.do"
@@ -95,7 +98,9 @@ def wechatNotice(SCKey, message):
 
 
 if __name__ == '__main__':
-    msg = sign(os.environ["SCHOOL_ID"], os.environ["PASSWORD"], os.environ["AUTO_POSITION"], os.environ["VACCINE"])
+    # msg = sign(os.environ["SCHOOL_ID"], os.environ["PASSWORD"], os.environ["AUTO_POSITION"], os.environ["VACCINE"])
+    msg = sign(school_id="22101002",password="130410",auto_position='浙江省 杭州市 拱墅区',vaccine='已接种第三针( booster jab received)')
+
     print(msg)
-    if os.environ["SCKEY"] != '' and msg != '打卡成功':
-        wechatNotice(os.environ["SCKEY"], msg)
+    # if os.environ["SCKEY"] != '' and msg != '打卡成功':
+    #     wechatNotice(os.environ["SCKEY"], msg)
